29dca96d7ef1342973dfa53c71e17828f7c4f2da
parviz001 <54933899+parviz001@users.noreply.github.com>
2019-09-15 04:08:32 +0400

    Add files via upload

---
+++ lab2.1.py
@@ -0,0 +1,14 @@
+import math
+while True:
+    print("Введите координаты точки x1")
+    x1=float (input())
+    print("Введите координаты точки y1")
+    y1=float (input())
+    print("Введите координаты точки x2")
+    x2=float (input())
+    print("Введите координаты точки y2")
+    y2=float (input())
+    print("Длина=" + str(math.sqrt((x1-x2)**2+(y1-y2)**2)))
+    
+    
+    
